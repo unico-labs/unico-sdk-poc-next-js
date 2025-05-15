91d241b3238859f457f1feaee6beef4e6e1f72a7
Kaique Pinheiro <kaique.pinheiro@unico.io>
2025-05-15 09:29:20 -0300

    feat: create new style guide
    # Description
    
    feat(style-guide): create new style guide for SDK Next.js
    
    Implemented a centralized mini Design System based on Unico's official branding guidelines, including:
    
    - Official Unico color palette with reusable color tokens
    - Standardized typography system with text variants (headings, subtitles, body, etc.)
    - Reusable basic UI components (Button, Input, Card, etc.)
    - Unified iconography system using a consistent icon library
    - Modular and scalable styling for easy maintenance across POCs
    - Inclusion of all options for document capture
    
    This aims to ensure visual consistency and scalability in SDK By Client POCs.
    
    # Demand
    
    What demand does this Pull Request refer to?
    
    - [ ] Bug
    - [X] Feature
    - [X] update

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import VerificationLayout from '@/components/VerificationLayout';
+
+export default function Home() {
+  return (
+    <VerificationLayout />
+  );
+}
---
+++ components/types/unico-webframe.d.ts
@@ -0,0 +1,8 @@
+declare module 'unico-webframe' {
+
+    const content: any;
+  
+    export = content;
+  
+  }
+  
